fba69dc72f8e6206c523e86ed2a28074d11522a0
inaenomaki <makiofinae@gmail.com>
2016-01-07 18:02:13 +0900

    first commit

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "default output" << std::endl;
+	return 0;
+}
+
+//change
